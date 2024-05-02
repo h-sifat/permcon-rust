29d5f00dc15ea91ea62052c495f7b5526ac974d8
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2024-05-02 07:34:43 +0400

    feat: add Octal parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
-pub mod permcon {}
+#[allow(unused)]
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    pub static ref OCTAL_PATTERN: Regex =
+        Regex::new(r"(?x)^(?P<special>[0-7])?(?P<user>[0-7])(?P<group>[0-7])(?P<other>[0-7])$")
+            .unwrap();
+    pub static ref SYMBOLIC_PATTERN: Regex =
+        Regex::new(r"^[bcdlps-]?[r-][w-][xsS-][r-][w-][xsS-][r-][w-][xtT-]$").unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,46 @@
 #[allow(unused)]
-use permcon2::*;
+use permcon::OCTAL_PATTERN;
+
+const PERMISSION_ORDER: [&str; 4] = ["special", "user", "group", "other"];
+
+#[derive(Debug)]
+struct Octal {
+    special: u8,
+    user: u8,
+    group: u8,
+    other: u8,
+}
+
+impl Octal {
+    fn from_array(values: [u8; 4]) -> Self {
+        let [special, user, group, other] = values;
+
+        Octal {
+            special,
+            user,
+            group,
+            other,
+        }
+    }
+
+    fn from_str(permission: &str) -> Result<Self, String> {
+        if !OCTAL_PATTERN.is_match(permission) {
+            return Err(String::from("Invalid octal permission."));
+        }
+
+        let caps = OCTAL_PATTERN.captures(permission).unwrap();
+
+        let values = PERMISSION_ORDER.map(|group_name| {
+            caps.name(group_name)
+                .map_or(0, |val| val.as_str().parse::<u8>().unwrap())
+        });
+
+        Ok(Octal::from_array(values))
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let perm = Octal::from_str("7666");
+
+    println!("{:?}", perm);
 }
